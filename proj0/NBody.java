848e827d00a30c676894141e938074536c682d18
passenger <732632827@qq.com>
2021-03-02 02:19:50 +0800

    completed proj0

---
+++ proj0/NBody.java
@@ -0,0 +1,126 @@
+public class NBody{
+    public static double readRadius(String filename){
+        In in = new In(filename);
+        int first = in.readInt();
+		double second = in.readDouble();
+        return second;
+    }
+    
+    public static Planet[] readPlanets(String filename){
+        In in = new In(filename);
+        int first = in.readInt();
+		double second = in.readDouble();
+        
+        Planet[] Planets = new Planet[first];
+        
+        for(int i=0; i<first; i++){
+            double xxPos = in.readDouble();
+            double yyPos = in.readDouble();
+            double xxVel = in.readDouble();
+            double yyVel = in.readDouble();
+            double mass = in.readDouble();
+            String imgFileName = in.readString();
+            Planet p = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
+            Planets[i] = p;
+        }
+        
+        return Planets;
+    }
+    
+    public static void drawBg(String imageToDraw, double radius) {
+		/** Sets up the universe so it goes from 
+		  * -100, -100 up to 100, 100 */
+		StdDraw.setScale(-radius, radius);
+
+		/* Clears the drawing window. */
+		StdDraw.clear();
+
+		/* Stamps three copies of advice.png in a triangular pattern. */
+        StdDraw.picture(0, 0, imageToDraw);
+		//StdDraw.picture(0, 75, imageToDraw);
+		//StdDraw.picture(-75, -75, imageToDraw);
+		//StdDraw.picture(75, -75, imageToDraw);
+
+		/* Shows the drawing to the screen, and waits 2000 milliseconds. */
+		StdDraw.show();
+		//StdDraw.pause(200);		
+	}
+    
+    public static void main(String[] args){
+        /** Collecting All Needed Input */
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        double radius = readRadius(filename);
+        Planet[] planets = readPlanets(filename);
+        
+        StdDraw.enableDoubleBuffering();
+        
+        /** Drawing the Background */
+        String imageToDraw = "images/starfield.jpg";
+        drawBg(imageToDraw, radius);
+        
+        /** Drawing All of the Planets */
+        for(Planet p: planets){
+            p.draw();
+        }
+        
+        double t = 0.0;
+        while(t<T){
+            double[] xForces = new double[planets.length];
+            double[] yForces = new double[planets.length];
+            for(int i=0; i<planets.length; i++){
+                double pNetFX = planets[i].calcNetForceExertedByX(planets);
+                double pNetFY = planets[i].calcNetForceExertedByY(planets);
+                xForces[i] = pNetFX;
+                yForces[i] = pNetFY;
+            }
+            
+            for(int i=0; i<planets.length; i++){
+                planets[i].update(dt, xForces[i], yForces[i]);
+            }
+            
+            drawBg(imageToDraw, radius);
+            
+            for(Planet p: planets){
+                p.draw();
+            }
+            
+            StdDraw.show();
+            StdDraw.pause(100);
+            //StdDraw.pause(10);
+            
+            t += dt;
+        }
+        
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                  planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                  planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ proj0/Planet.java
@@ -0,0 +1,94 @@
+public class Planet{
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    public static final double gravity = 6.67e-11;
+    
+    public Planet(double xxPos, double yyPos, double xxVel,double yyVel, double mass, String imgFileName){
+        this.xxPos = xxPos;
+        this.yyPos = yyPos;
+        this.xxVel = xxVel;
+        this.yyVel = yyVel;
+        this.mass = mass;
+        this.imgFileName = imgFileName;
+    }
+    
+    public Planet(Planet p){
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+    
+    public double calcDistance(Planet p){
+        return Math.sqrt((this.xxPos-p.xxPos)*(this.xxPos-p.xxPos)+(this.yyPos-p.yyPos)*(this.yyPos-p.yyPos));
+    }
+    
+    public double calcForceExertedBy(Planet p){
+        return gravity*p.mass*this.mass/(calcDistance(p)*calcDistance(p));
+    }
+    
+    public double calcForceExertedByX(Planet p){
+        return calcForceExertedBy(p)*(p.xxPos-this.xxPos)/calcDistance(p);
+    }
+    
+    public double calcForceExertedByY(Planet p){
+        return calcForceExertedBy(p)*(p.yyPos-this.yyPos)/calcDistance(p);
+    }
+    
+    public double calcNetForceExertedByX(Planet[] p){
+        double netFX = 0;
+        for(Planet planet: p){
+            if(equals(planet)==false){
+                netFX+=calcForceExertedByX(planet);
+            }
+        }
+        return netFX;
+    }
+    
+    public double calcNetForceExertedByY(Planet[] p){
+        double netFY = 0;
+        for(Planet planet: p){
+            if(equals(planet)==false){
+                netFY+=calcForceExertedByY(planet);
+            }
+        }
+        return netFY;
+    }
+    
+    public void update(double dt, double xForce, double yForce){
+        double ax = xForce / this.mass;
+        double ay = yForce / this.mass;
+        this.xxVel += ax*dt;
+        this.yyVel += ay*dt;
+        this.xxPos += dt*this.xxVel;
+        this.yyPos += dt*this.yyVel;
+    }
+    
+    /** Drawing One Planet */
+    public void draw(){
+        StdDraw.picture(this.xxPos, this.yyPos, "images/"+imgFileName);
+		StdDraw.show();
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
